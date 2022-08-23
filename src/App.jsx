e12d0d0b8fe2679d31852d7cdf97f11c3f509f6e
Barbosa885 <80699703+Barbosa885@users.noreply.github.com>
2022-08-23 19:23:13 -0300

    :sparkles: Add the fonts folder and apply the font in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function App() {
 
   return (
-    <div cla>
-      <h1>Hello World!</h1>
+    <div className="bg-gray-900 h-screen">
+      <h1 className="text-6xl text-white font-SFpro font-semibold p-10">TODO</h1>      
     </div>
   )
 }
 
-export default App
+export default App;
