5e4901c6efbf209ad7f0a9145f8d66465cff7592
Barbosa885 <80699703+Barbosa885@users.noreply.github.com>
2022-08-27 16:50:55 -0300

    :sparkles: Creates the input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Input from "./components/Input.jsx"
 
 function App() {
 
   return (
     <div className="bg-gray-900 h-screen">
       <h1 className="text-6xl text-white font-SFpro font-semibold p-10">TODO</h1>      
+      <div className="flex justify-center ">
+        <Input />
+      </div>
     </div>
   )
 }
