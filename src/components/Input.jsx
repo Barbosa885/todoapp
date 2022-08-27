7b442ce5b7ed25ee6439c2074b05f58ccb47c87f
Barbosa885 <80699703+Barbosa885@users.noreply.github.com>
2022-08-27 17:49:38 -0300

    :lipstick:Update the input style

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,17 +3,15 @@ import React from "react";
 function InputText( ){
 
   return (
-    <label class="relative block">
-      <span class="sr-only">Search</span>
-        <button class="w-10 absolute inset-y-0 left-0 flex bg-blue-400 active:bg-blue-500 rounded justify-center">
+    <label className="relative block">
+      <label className="w-10 absolute inset-y-0 left-0 flex bg-blue-400 hover:bg-blue-500 rounded justify-center cursor-pointer">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 self-center text-slate-800">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
         </svg>
-      </button>
-      <input class="placeholder:italic placeholder:text-slate-400 bg-white w-full border border-slate-300 rounded-md py-2 pl-20 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Add some task..." type="text" name="search"/>
+      </label> 
+    <input type="text" placeholder="Add some task..." className="placeholder:text-slate-300 placeholder:italic placeholder:text-base rounded-md sm:text-sm py-2 pl-12 flex focus:outline-none focus:border-0"></input>
     </label>
   );
-
 };
 
 export default InputText;
